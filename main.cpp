577f5977049ad23161f46a93b6752bcddacb7917
Brianmax <george.maxi@ucsp.edu.pe>
2024-04-12 20:00:49 -0500

    first app qt

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "QGuiApplication"
+#include "QtQml/QQmlApplicationEngine"
+int main(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/untitled/qml/main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl){
+        if(!obj and url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+    return app.exec();
+}
